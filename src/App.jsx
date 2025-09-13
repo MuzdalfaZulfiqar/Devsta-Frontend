7e7a9a30a3d3f7528387b6f2ad0dc1374885d09b
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-09-14 00:09:28 +0500

    Fixed github connection modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/User/Login";
 import Signup from "./pages/User/Signup";
 import Dashboard from "./pages/User/Dashboard";
+import GithubConnectModal from "./components/dashboard/GithubConnectModal";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-black text-white">
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/" element={<GithubConnectModal />} /> */}
         </Routes>
       </div>
     </Router>
