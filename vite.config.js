64f8aaa5edda8d74aaf9901200df07e83982b58a
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2026-02-08 23:16:23 +0500

    updated vercel files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './', 
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-})
+  base: './', // works for root deployment
+  build: {
+    outDir: 'dist', // ensure output folder matches Vercel
+  },
+});
